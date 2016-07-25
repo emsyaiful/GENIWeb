85197fb74afe17406acb8003208ca068b2f5649d
emsyaiful <jihad.amrulloh@gmail.com>
2016-07-25 13:34:04 +0700

    edit git ignore

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,8 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Models\Userlist;
 
 class UserController extends Controller
 {
-    //
+    public function getUser(){
+    	$user = new Userlist;
+    	$user->get();
+
+    	print_r($user);
+    }
 }
