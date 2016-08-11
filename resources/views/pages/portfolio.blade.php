a6bfdb4760a56e24a31f2aac7bc1088af976759f
Moch. Imam Zarqoni <imamzarqoni95@gmail.com>
2016-08-11 16:43:25 +0700

    update portfolio.blade.php

--- resources/views/pages/portfolio.blade.php
+++ resources/views/pages/portfolio.blade.php
@@ -3,19 +3,15 @@
 		<a class="close-folio-item" href="#"><i class="fa fa-times"></i></a>
 		@foreach($data as $berita)
 		<div class="row">
-			<div class="col-sm-6">
-				<img class="img-responsive" src=[[ $berita['news_image'] ]] alt="">
+			<div class="col-sm-4">
+				<img class="img-responsive" src="[[ $berita['news_image'] ]]" alt="">
+				<p><span>Date:</span>[[ $berita['news_timecreated'] ]]</p>
 			</div>
-			<div class="col-sm-6">
+			<div class="col-sm-8">
 				<div class="project-info">
 					<h3>[[ $berita['news_title'] ]]</h3>
 					{!! html_entity_decode($berita['news_content']) !!}
 				</div>
-			</div>
-		</div>
-		<div class="row">
-			<div class="project-details">
-				<p><span>Date:</span>[[ $berita['news_timecreated'] ]]</p>
 			</div>
 		</div>
 		@endforeach
